62b79364a8bf12649cfb8af4875baee35b727900
Duccio Rocchini <ducciorocchini@gmail.com>
2026-03-31 17:16:16 +0200

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -3,6 +3,9 @@
 library(terra)
 library(imageRy)
 library(viridis)
+library(ggplot2)
+# install.packages("ggrdiges")
+library(ggridges)
 
 im.list()
 
@@ -46,5 +49,38 @@ plot(dif)
 
 # RGB
 im.plotRGB(gr, r=1, g=2, b=4)
+
+# NDVI data
+ndvi <- im.import("Sentinel2_NDVI_2020")
+
+# istogrammi
+hist(ndvi)
+
+# ridgeline plotting
+im.ridgeline(ndvi, scale=1, palette="viridis")
+
+names(ndvi) <- c("02_feb", "05_may", "08_aug", "11_nov")
+
+# pairs
+pairs(ndvi)
+
+plot(ndvi[[1]], ndvi[[2]])
+
+# y = x
+# y = a + bx
+# y = 0 + 1x = x 
+# a = 0
+# b = 1
+
+# insert the line x=y 
+abline(0, 1)
+
+plot(ndvi[[1]], ndvi[[2]], xlim=c(-0.3,0.9), ylim=c(-0.3,0.9))
+abline(0, 1)
+
+
+
+
+
 
 
