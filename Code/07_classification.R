b46e606c09e613520ceb6e3189584ae84b1f2a62
Duccio Rocchini <ducciorocchini@gmail.com>
2026-04-21 17:50:36 +0200

    Update 07_classification.R

--- Code/07_classification.R
+++ Code/07_classification.R
@@ -37,8 +37,53 @@ plot(dji)
 # classify data
 djic <- im.classify(dji, num_clusters=2)
 
+# Mato grosso examples
+im.list()
 
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg") 
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")    
 
+im.multiframe(2,1)
+plot(m1992)
+plot(m2006)
+
+# classification
+m1992c <- im.classify(m1992, seed=42, num_clusters=2)
+
+# Assign labels
+levels(m1992c) <- data.frame(
+  value = c(1, 2),
+  label = c("forest", "human")
+)
+
+m2006c <- im.classify(m2006, seed=42, num_clusters=2)
+
+# Assign labels
+levels(m2006c) <- data.frame(
+  value = c(2, 1),
+  label = c("forest", "human")
+)
+
+# Calculating frequencies
+f1992 <- freq(m1992c) 
+
+prop1992 <- f1992$count / ncell(m1992c)
+
+perc1992 <- prop1992 * 100
+
+# Calculating frequencies
+f2006 <- freq(m2006c) 
+
+prop2006 <- f2006$count / ncell(m2006c)
+
+perc2006 <- prop2006 * 100
+
+# Table
+tabout <- data.frame(
+  class=c("Forest","Human"),
+  perc1992=c(83, 17),
+  perc2006=c(45, 55)
+  )
 
 
 
