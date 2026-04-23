ada0f0b5688244bf0ae048094597f52ee9bf3ab2
Duccio Rocchini <ducciorocchini@gmail.com>
2026-04-23 15:43:38 +0200

    Create 08_functions.R

---
+++ Code/08_functions.R
@@ -0,0 +1,14 @@
+# My functions!
+
+somma <- function(x,y){
+  z=x+y
+  return(z)
+  }
+
+# Exercise: make the functuion called difference
+
+differenzaselvaggia <- function(x,y){
+  z=x-y
+  return(z)
+  }
+
